5789fe01a9f4b46e7b18982ed29eaafd19d58d47
abelsonlive <brianabelson@gmail.com>
2014-01-08 01:48:21 -0500

    okay, lets try this now

--- dymo.py
+++ dymo.py
@@ -11,53 +11,53 @@ rdb = redis.from_url(redis_url)
 
 # intitialize app
 app = Flask(__name__)
+
+# # test index page
+# @app.route('/')
+# def index():
+#   return render_template('index.html')
+
 # index page
 @app.route('/')
 def index():
-  return render_template('index.html')
 
-# # index page
-# @app.route('/')
-# def index():
+  # serve a random image that we haven't labeled yet
+  completed = rdb.keys()
+  images_to_label = [i for i in images if i not in frozenset(completed)]
+  image = choice(images_to_label)
 
-#   # serve a random image that we haven't labeled yet
-#   completed = rdb.keys()
-#   images_to_label = [i for i in images if i not in frozenset(completed)]
-#   image = choice(images_to_label)
+  return render_template('home.html', image = image, images_left = len(images_to_label))
 
-#   return render_template('home.html', image = image, images_left = len(images_to_label))
-
-# # form post for label data
-# @app.route('/label', methods=['POST'])
-# def label_image(): 
+# form post for label data
+@app.route('/label', methods=['POST'])
+def label_image(): 
   
-#   # parse form
-#   value = json.loads(request.form.copy()['data'])
+  # parse form
+  value = json.loads(request.form.copy()['data'])
   
-#   # extract key
-#   key = value['image']
+  # extract key
+  key = value['image']
 
-#   # push to redis
-#   rdb.set(key, json.dumps(value))
+  # push to redis
+  rdb.set(key, json.dumps(value))
 
-#   # redirect to a new image
-#   return redirect(url_for('index'))
+  # redirect to a new image
+  return redirect(url_for('index'))
 
-# @app.after_request
-# def add_header(responase):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-#     response.headers['Cache-Control'] = 'public, max-age=600'
-#     return response
+@app.after_request
+def add_header(responase):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
 
-
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     """Custom 404 page."""
-#     return render_template('404.html'), 404
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
