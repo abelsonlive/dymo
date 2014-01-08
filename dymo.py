b1457cbad36ba27b7324a511f02476c80c83a6d9
abelsonlive <brianabelson@gmail.com>
2014-01-08 02:00:08 -0500

    simplifying

--- dymo.py
+++ dymo.py
@@ -9,7 +9,7 @@ images = [i.strip() for i in os.listdir('static/images') if i != '' and i is not
 # # initialize redis
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 rdb = redis.from_url(redis_url)
-
+rdb.set("test", "woot")
 # intitialize app
 app = Flask(__name__)
 app.root_path = abspath(dirname(__file__))
@@ -24,11 +24,13 @@ app.root_path = abspath(dirname(__file__))
 def index():
 
   # serve a random image that we haven't labeled yet
-  completed = rdb.keys()
-  images_to_label = [i for i in images if i not in frozenset(completed)]
-  image = choice(images_to_label)
+  # completed = rdb.keys()
+  # images_to_label = [i for i in images if i not in frozenset(completed)]
+  # image = choice(images_to_label)
+  # return render_template('home.html', image = image, images_left = len(images_to_label))
 
-  return render_template('home.html', image = image, images_left = len(images_to_label))
+  v = rdb.get("test")
+  return v
 
 # form post for label data
 @app.route('/label', methods=['POST'])
@@ -45,21 +47,6 @@ def label_image():
 
   # redirect to a new image
   return redirect(url_for('index'))
-
-@app.after_request
-def add_header(responase):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
